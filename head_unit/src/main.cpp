7abb0e3ce7a93eb6c74443208076d3f9efb738b1
mczb <12190588@inha.edu>
2023-09-14 23:01:14 +0900

    Change Directory Configuration

--- head_unit/src/main.cpp
+++ head_unit/src/main.cpp
@@ -4,14 +4,14 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QtWebEngine::initialize();
-
     QGuiApplication app(argc, argv);
+
+    QtWebEngine::initialize();
 
     qmlRegisterType<YouTubeSearch>("com.example", 1, 0, "YouTubeSearch");
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/MainLayout.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/qml/MainLayout.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
