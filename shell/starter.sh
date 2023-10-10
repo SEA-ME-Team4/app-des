b43a95690c9a3da47ef20fb9858d6913a9af2bd9
mczb <12190588@inha.edu>
2023-10-11 00:25:12 +0900

    Modify for shell variable

--- shell/starter.sh
+++ shell/starter.sh
@@ -10,11 +10,17 @@ check_and_run()
     local init_count=$(ps -ef|grep -w ./execute/$1|grep -v 'grep'|grep -v $0|wc -l)
     if [ ${init_count} -eq 0 ]
     then
-        echo $1 is Not Running
-        nohup ./execute/$1 </dev/null >/dev/null 2>&1 &
-        disown
-        echo Running $1 Automatically
-        sleep 1
+        local is_file=$(ls ./execute|grep -w $1|grep -v 'grep'|wc -l)
+        if [ ${is_file} -eq 0 ]
+        then
+            echo $1 is Not in execute folder
+        else
+            echo $1 is Not Running
+            nohup ./execute/$1 </dev/null >/dev/null 2>&1 &
+            disown
+            echo Running $1 Automatically
+            sleep 1
+        fi
 
         local final_count=$(ps -ef|grep -w ./execute/$1|grep -v 'grep'|grep -v $0|wc -l)
         if [ ${final_count} -eq 0 ]
@@ -27,24 +33,31 @@ check_and_run()
         fi
     else
         echo $1 is Already Running
-        echo Succeed
-        Succeed+=($1)
+        echo Pass
+        Pass+=($1)
     fi   
     echo
 }
 
+Pass=()
 Succeed=()
 Failed=()
 
-check_and_run instrument_cluster
-check_and_run head_unit
-check_and_run input
-check_and_run speed
-check_and_run battery
-check_and_run racer
-check_and_run exception_handler
+if [ -z $1 ]
+then
+    check_and_run instrument_cluster
+    check_and_run head_unit
+    check_and_run input
+    check_and_run speed
+    check_and_run battery
+    check_and_run racer
+    check_and_run exception_handler
+else
+    check_and_run $1
+fi
 
 echo [Summary]
+echo ${#Pass[@]} Pass : ${Pass[@]}
 echo ${#Succeed[@]} Running : ${Succeed[@]}
 echo ${#Failed[@]} Failed : ${Failed[@]}
 echo
