26f8f492e49673bcbc2a37ad52d5231316e74978
ho <ws000617@gmail.com>
2023-10-12 14:27:36 +0200

    SSl test

--- head_unit/src/youtubesearch.cpp
+++ head_unit/src/youtubesearch.cpp
@@ -6,6 +6,10 @@ YouTubeSearch::YouTubeSearch() {
 }
 
 void YouTubeSearch::searchVideos(const QString& query) {
+    QSslConfiguration conf = request.sslConfiguration();
+    conf.setPeerVerifyMode(QSslSocket::VerifyNone);
+    request.setSslConfiguration(conf);
+
     manager->get(QNetworkRequest(QUrl("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + query + "&maxResults=5&key=AIzaSyC2CGURgrCoILPCAUa2OGE_fuK3AyYYBa4")));
 }
 
