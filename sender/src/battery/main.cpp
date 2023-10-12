f639a16f17a1a882022e79de6985e9941d5f3277
mczb <12190588@inha.edu>
2023-10-13 00:29:54 +0900

    Add Battery percentage Formula

--- sender/src/battery/main.cpp
+++ sender/src/battery/main.cpp
@@ -2,11 +2,14 @@
 #include "battery.h"
 
 int main() {
+    uint8_t battery_percent = 0; 
+
     Piracer piracer = Piracer();
     Battery battery = Battery();
     
     while (1) {
-        battery.setBattery(piracer.getVoltage());
+        battery_percent = ((((piracer.getVoltage() / 3) - 3.1) / 1.1) * 100);
+        battery.setBattery(battery_percent);
         usleep(10000);
     }   
 }
