a83b8331598ca8d54df42cfad9e65caef9dbaf5d
mczb <12190588@inha.edu>
2023-10-13 22:48:27 +0900

    Update starter.sh

--- tools/starter.sh
+++ tools/starter.sh
@@ -16,8 +16,8 @@ check_and_run()
             echo $1 is Not Running
             if [ $1 = "head_unit" ]
             then
-                echo Setting time from time.google.com
-                rdate -s time.google.com
+                echo Setting time time.bora.net
+                rdate -s time.bora.net
                 echo $(date)
                 nohup ./execute/$1 -platform eglfs --no-sandbox </dev/null >/dev/null 2>&1 &
             elif [ $1 = "instrument_cluster" ]
