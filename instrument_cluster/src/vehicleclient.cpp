199a03dd2ddaf661a2c7f8cb1bdcd4a551426463
mczb <12190588@inha.edu>
2023-10-13 21:07:00 +0900

    Update vehicleclient.cpp

--- instrument_cluster/src/vehicleclient.cpp
+++ instrument_cluster/src/vehicleclient.cpp
@@ -28,16 +28,6 @@ void VehicleClient::speedProxyInit() {
     while (!speedProxy->isAvailable())
         usleep(10);
     std::cout << "Available..." << std::endl;
-
-    CommonAPI::CallStatus callStatus;
-    int16_t value = 0;
-    CommonAPI::CallInfo info(4000);
-    speedProxy->getSpeedAttribute().getValue(callStatus, value, &info);
-    if (callStatus != CommonAPI::CallStatus::SUCCESS) {
-        std::cerr << "Remote call Speed failed!\n";
-    }
-    std::cout << "Got Speed: " << (int)value << std::endl;
-
     speedProxy->getSpeedAttribute().getChangedEvent().subscribe([&](const int16_t& speed) {
         // qDebug()<<speed;
         emit speedChanged(speed);
@@ -49,16 +39,6 @@ void VehicleClient::batteryProxyInit() {
     while (!batteryProxy->isAvailable())
         usleep(10);
     std::cout << "Available..." << std::endl;
-    
-    CommonAPI::CallStatus callStatus;
-    uint8_t value = 0;
-    CommonAPI::CallInfo info(5000);
-    batteryProxy->getBatteryAttribute().getValue(callStatus, value, &info);
-    if (callStatus != CommonAPI::CallStatus::SUCCESS) {
-        std::cerr << "Remote call Battery failed!\n";
-    }
-    std::cout << "Got Battery: " << (int)value << std::endl;
-
     batteryProxy->getBatteryAttribute().getChangedEvent().subscribe([&](const uint8_t& battery) {
         // qDebug()<<battery;
         emit batteryChanged(battery);
@@ -70,16 +50,6 @@ void VehicleClient::brakeProxyInit() {
     while (!brakeProxy->isAvailable())
         usleep(10);
     std::cout << "Available..." << std::endl;
-    
-    CommonAPI::CallStatus callStatus;
-    bool value = 0;
-    CommonAPI::CallInfo info(6000);
-    brakeProxy->getBrakeAttribute().getValue(callStatus, value, &info);
-    if (callStatus != CommonAPI::CallStatus::SUCCESS) {
-        std::cerr << "Remote call Brake failed!\n";
-    }
-    std::cout << "Got Brake: " << (int)value << std::endl;
-
     brakeProxy->getBrakeAttribute().getChangedEvent().subscribe([&](const bool& brake) {
         // qDebug()<<brake;
         emit brakeChanged(brake);
@@ -91,16 +61,6 @@ void VehicleClient::gearProxyInit() {
     while (!gearProxy->isAvailable())
         usleep(10);
     std::cout << "Available..." << std::endl;
-
-    CommonAPI::CallStatus callStatus;
-    uint8_t value = 0;
-    CommonAPI::CallInfo info(8000);
-    gearProxy->getGearAttribute().getValue(callStatus, value, &info);
-    if (callStatus != CommonAPI::CallStatus::SUCCESS) {
-        std::cerr << "Remote call Gear failed!\n";
-    }
-    std::cout << "Got Gear: " << (int)value << std::endl;
-
     gearProxy->getGearAttribute().getChangedEvent().subscribe([&](const uint8_t& gear) {
         // qDebug()<<gear;
         emit gearChanged(gear);
