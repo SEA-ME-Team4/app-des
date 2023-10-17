27088354555b2d3387c658b7f57d0495a2545255
mczb <12190588@inha.edu>
2023-10-17 10:28:41 +0900

    Develop without racer and speed

---
+++ sender/battery/tools/battery.sh
@@ -0,0 +1,3 @@
+export COMMONAPI_CONFIG=./config/commonapi.ini
+# nohup ./execute/battery </dev/null >/dev/null 2>&1 &
+./execute/battery
---
+++ sender/input/tools/input.sh
@@ -0,0 +1,3 @@
+export COMMONAPI_CONFIG=./config/commonapi.ini
+# nohup ./execute/input </dev/null >/dev/null 2>&1 &
+./execute/input
