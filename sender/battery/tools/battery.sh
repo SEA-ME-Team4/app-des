ce0d6a1da79d030702789ff46e1b9fd4c2ee203e
mczb <12190588@inha.edu>
2023-10-18 23:28:07 +0900

    Modify Shell Scripts

--- sender/battery/tools/battery.sh
+++ sender/battery/tools/battery.sh
@@ -1,5 +1,1 @@
-INIT_DIR=${pwd}
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-cd ${SCRIPT_DIR}
-./execute/battery
-cd ${INIT_DIR}
+./battery/execute/battery
--- sender/input/tools/input.sh
+++ sender/input/tools/input.sh
@@ -1,5 +1,1 @@
-INIT_DIR=${pwd}
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-cd ${SCRIPT_DIR}
-./execute/input
-cd ${INIT_DIR}
+./input/execute/input
