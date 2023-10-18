30fc5eda6a3e3ea04fbb97692d6114dbb6e59d9a
mczb <12190588@inha.edu>
2023-10-19 00:09:47 +0900

    Add Battery Debug Lines

--- sender/battery/src/main.cpp
+++ sender/battery/src/main.cpp
@@ -1,6 +1,8 @@
 #include "piracer.h"
 #include "battery.h"
 
+#include <iostream>
+
 int main() {
     uint8_t battery_percent = 0; 
 
@@ -8,7 +10,9 @@ int main() {
     Piracer piracer = Piracer();
     
     while (1) {
+        std::cout<<"voltage : "<<piracer.getVoltage()<<std::endl;
         battery_percent = ((((piracer.getVoltage() / 3) - 3.1) / 1.1) * 100);
+        std::cout<<"percent : "<<battery_percent<<std::endl;
         battery.setBattery(battery_percent);
         usleep(10000);
     }   
