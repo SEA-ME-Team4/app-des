e352d4cb2af3d7279d9bf11f5e3d000fa7a2d6a6
mczb <12190588@inha.edu>
2023-10-20 02:24:13 +0900

    Add Brake & Speed Proxy to Head Unit

--- head_unit/src/vehiclestatus.cpp
+++ head_unit/src/vehiclestatus.cpp
@@ -7,6 +7,13 @@ VehicleStatus::VehicleStatus() {
     gearService = std::make_shared<GearStatusStubImpl>();
     gearServiceInit();
     sendGear(gear);
+
+    brakeProxy = runtime->buildProxy<BrakeStatusProxy>("local", "BrakeStatus", "HeadUnit_Brake_Proxy");
+    brakeProxyInit();
+
+    speedProxy = runtime->buildProxy<SpeedStatusProxy>("local", "SpeedStatus", "HeadUnit_Speed_Proxy");
+    speedProxyInit();
+
 }
 
 VehicleStatus::~VehicleStatus() {
@@ -23,4 +30,24 @@ void VehicleStatus::gearServiceInit() {
     }
     std::cout << "Successfully Registered Service!" << std::endl;
     gear = 0;
+}
+
+void VehicleStatus::brakeProxyInit() {
+    std::cout << "Checking Brake availability!" << std::endl;
+    while (!brakeProxy->isAvailable())
+        usleep(10);
+    std::cout << "Available..." << std::endl;
+    brakeProxy->getBrakeAttribute().getChangedEvent().subscribe([&](const bool& brake) {
+        emit brakeChanged(brake);
+    });
+}
+
+void VehicleStatus::speedProxyInit() {
+    std::cout << "Checking Speed availability!" << std::endl;
+    while (!speedProxy->isAvailable())
+        usleep(10);
+    std::cout << "Available..." << std::endl;
+    speedProxy->getSpeedAttribute().getChangedEvent().subscribe([&](const int16_t& speed) {
+        emit speedChanged(speed);
+    });
 }
