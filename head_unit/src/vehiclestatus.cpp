9fc41db19dfcbbb2a0ec4174b5b456249d08e61e
mczb <12190588@inha.edu>
2023-10-19 10:02:42 +0900

    Remove Brake and Exception Handler in Head Unit

--- head_unit/src/vehiclestatus.cpp
+++ head_unit/src/vehiclestatus.cpp
@@ -7,12 +7,6 @@ VehicleStatus::VehicleStatus() {
     gearService = std::make_shared<GearStatusStubImpl>();
     gearServiceInit();
     sendGear(gear);
-
-    brakeProxy = runtime->buildProxy<BrakeStatusProxy>("local", "BrakeStatus", "HeadUnit_Brake_Proxy");
-    brakeProxyInit();
-
-    errorProxy = runtime->buildProxy<ToApplicationProxy>("local", "ToApplication", "HeadUnit_Error_Proxy");
-    errorProxyInit();
 }
 
 VehicleStatus::~VehicleStatus() {
@@ -29,34 +23,4 @@ void VehicleStatus::gearServiceInit() {
     }
     std::cout << "Successfully Registered Service!" << std::endl;
     gear = 0;
-}
-
-void VehicleStatus::brakeProxyInit() {
-    std::cout << "Checking Brake availability!" << std::endl;
-    while (!brakeProxy->isAvailable())
-        usleep(10);
-    std::cout << "Available..." << std::endl;
-    brakeProxy->getBrakeAttribute().getChangedEvent().subscribe([&](const bool& brake) {
-        emit brakeChanged(brake);
-    });
-}
-
-void VehicleStatus::errorProxyInit() {
-    std::cout << "Checking Error Handling availability!" << std::endl;
-    while (!errorProxy->isAvailable())
-        usleep(10);
-    std::cout << "Available..." << std::endl;
-    errorProxy->getErrorEvent().subscribe([&](const std::string& name) {
-        if (name=="speed_error") {emit speedStatus(false);}
-        else if (name=="battery_error") {emit batteryStatus(false);}
-        else if (name=="input_error") {emit inputStatus(false);}
-        else if (name=="racer_error") {emit racerStatus(false);}
-        else if (name=="gear_error") {emit gearStatus(false);}
-
-        else if (name=="speed_okay") {emit speedStatus(true);}
-        else if (name=="battery_okay") {emit batteryStatus(true);}
-        else if (name=="input_okay") {emit inputStatus(true);}
-        else if (name=="racer_okay") {emit racerStatus(true);}
-        else if (name=="gear_okay") {emit gearStatus(true);}
-    });
 }
