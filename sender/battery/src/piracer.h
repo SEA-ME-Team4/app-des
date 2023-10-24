9bffd54a36c8643128765e997750b55c3236906a
mczb <12190588@inha.edu>
2023-10-24 23:42:02 +0900

    Erase set_piracer in Battery sender

--- sender/battery/src/piracer.h
+++ sender/battery/src/piracer.h
@@ -7,13 +7,9 @@ public:
     Piracer();
     ~Piracer();
     double getVoltage();
-    void setRacer(int gear, bool brake, float steering, float throttle);
-    void emergencyStop();
 
 private:
     PyObject *pModule, *pClass, *pInstance;
     PyObject *pVoltage;
     double voltage;
-    void set_steering_percent(float steering);
-    void set_throttle_percent(float throttle);
 };
